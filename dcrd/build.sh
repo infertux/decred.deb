2226f0d902203d8ca11d08c0315133c81284eeed
Cédric Félizard <cedric@felizard.fr>
2019-10-26 07:23:08 +0000

    Initial commit

---
+++ dcrd/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -euo pipefail
+
+version=${1:-1.5.0}
+iteration=${2:-1}
+
+cd "$(dirname "$0")"
+
+pushd dcrd.git
+    git pull
+    go build -v
+    pushd cmd/dcrctl
+        go build -v
+    popd
+popd
+
+cp -an dcrd.git/sampleconfig/sampleconfig.go dcrd.conf
+diff -u dcrd.git/sampleconfig/sampleconfig.go dcrd.conf || true
+echo "Does the diff look okay? ^C to abort"
+read -r
+
+cp -a dcrd.git/dcrd .
+strip dcrd
+cp -a dcrd.git/cmd/dcrctl/dcrctl .
+strip dcrctl
+
+fpm -f --verbose -s dir -t deb \
+    --name dcrd \
+    --provides dcrd \
+    --deb-systemd dcrd.service \
+    --description "Decred dcrd" \
+    --url "https://github.com/decred/dcrd" \
+    --license "ISC" \
+    --vendor "https://www.decred.org/" \
+    --maintainer "Cédric Felizard <cedric@felizard.eu>" \
+    --version "${version}" \
+    --iteration "${iteration}" \
+    --after-install ../create-user.sh \
+    --depends dcrctl \
+    --config-files /etc/decred/dcrd.conf \
+    --package dcrd.deb \
+    dcrd=/usr/bin/ \
+    dcrd.conf=/etc/decred/
