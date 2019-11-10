6693a5d678c6c824b9668e79337cbd94e66df5cf
Cédric Félizard <cedric@felizard.fr>
2019-11-10 05:40:20 +0000

    Package dcrwallet-1.5.0-rc1

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd "$(dirname "$0")"
+
+target="${1:-dcrd/dcrd-1.5.0}"
+container=deb-builder
+volume=/root/HOST
+
+docker pull debian:unstable
+
+[ "$(docker ps -qf "name=${container}")" ] || docker run --name $container -d -t -v "${PWD}:${volume}" debian:unstable
+
+docker exec $container apt-get update
+docker exec $container apt-get upgrade -y
+docker exec $container apt-get install -y devscripts dh-exec vim quilt lintian
+
+dir="${volume}/${target}"
+package=${target#*/}
+package="${package/-/_}*.deb"
+
+docker exec --workdir "$dir" $container debuild || docker exec -ti $container /bin/bash
+
+docker exec --workdir "$dir" $container find .. -type f -name "$package" -exec lintian --info --pedantic {} \;
+docker exec --workdir "$dir" $container find .. -type f -name "$package" -exec dpkg -c {} \;
+docker exec --workdir "$dir" $container find .. -type f -name "$package" -exec ls -lh {} \;
+docker exec --workdir "$dir" $container find .. -type f -name "$package" -exec sha256sum {} \;
+
+# docker exec --workdir "$dir" $container find dist -type f -name "$package" -exec dpkg -i {} \;
+# docker exec $container dpkg -L $package_name
+# docker exec $container dpkg -V $package_name
+
+sudo chown -R "${USER}:" ./*
+
+# docker stop $container
