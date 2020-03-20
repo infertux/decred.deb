b2132a81fd47dc891c7b05f60b9774fe0191a864
Cédric Félizard <cedric@felizard.fr>
2020-03-20 14:47:56 +0000

    Update build script to handle different version for stakepoold

--- build-all.sh
+++ build-all.sh
@@ -2,14 +2,24 @@
 
 set -euxo pipefail
 
+dcr_version=1.5.1
+stakepoold_version=1.5.0
+
 cd "$(dirname "$0")"
 
-version=1.5.1
+build() {
+    local target=$1
+    local version=$2
 
-for target in dcrctl dcrd dcrwallet stakepoold; do
     path="${target}/${target}-${version}"
-    ./build.sh $path
+    ./build.sh "$path"
+}
+
+for dcr in dcrctl dcrd dcrwallet; do
+    build $dcr $dcr_version
 done
+
+build stakepoold $stakepoold_version
 
 find . -name "*.deb" -exec sha256sum {} \;
 find . -name "*.deb" -exec ls -lh {} \;
